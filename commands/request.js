f32d3f12f3417f07b1d9acd85cd00871eaafa0a3
null1017 <nonick.mc1017@gmail.com>
2022-03-30 01:32:22 +0900

    Requestコマンド -5
    ・requrstCh以外でコマンドが使用できないように対策

--- commands/request.js
+++ commands/request.js
@@ -2,7 +2,7 @@
 // エディションとMCIDを取得させ、申請を申請側とMODチャンネル側に送信する。
 const { SlashCommandBuilder, channelMention } = require('@discordjs/builders');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const { requestch, modch } = require('../config.json');
+const { requestCh, modCh } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -26,38 +26,48 @@ module.exports = {
 		const edition = interaction.options.getString('edition');
 		const mcid = interaction.options.getString('mcid');
 		// コマンドを打った人の情報を取得
-		const userid = interaction.user.id;
-		const useravater = interaction.user.displayAvatarURL();
+		const userId = interaction.user.id;
+		const userAvater = interaction.user.displayAvatarURL();
+		// コマンドを打ったチャンネルのIDを取得
+		const sendCh = interaction.channnelId
 
-		// 申請側にメッセージを送信
-		const embed = new MessageEmbed()
-			.setColor(`#5662F6`)
-			.setTitle('申請完了')
-			.setThumbnail(useravater)
-			.setDescription('以下の情報で申請を送信しました。\n__Tips:登録には時間がかかる場合があります。__')
-			.addFields(
-				{ name: 'エディション', value: `${edition}版`, inline: true },	
-				{ name: 'MCID', value: `${mcid}`, inline: true }
-			);
-		await interaction.reply({ embeds: [embed] });
+		if (sendCh === requestCh) {
+			// 申請側にメッセージを送信
+			const embed = new MessageEmbed()
+				.setColor(`#5662F6`)
+				.setTitle('申請完了')
+				.setThumbnail(userAvater)
+				.setDescription('以下の情報で申請を送信しました。\n__Tips:登録には時間がかかる場合があります。__')
+				.addFields(
+					{ name: 'エディション', value: `${edition}版`, inline: true },	
+					{ name: 'MCID', value: `${mcid}`, inline: true }
+				);
+			await interaction.reply({ embeds: [embed] });
 
-		// MODチャンネル側にメッセージを送信
-		const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('hail')
-					.setLabel('( ᐛ )')
-					.setStyle('PRIMARY'),
-			);
-		const embed_mod = new MessageEmbed()
-			.setColor(`#56B482`)
-			.setTitle('新しい申請が送信されました!')
-			.setDescription(`申請者:<@${userid}>`)
-			.setThumbnail(useravater)
-			.addFields(
-				{ name: 'エディション', value: `${edition}版`, inline: true },	
-				{ name: 'MCID', value: `${mcid}`, inline: true }
-			);
-		await interaction.guild.channels.cache.get(modch).send({ embeds: [embed_mod], components: [row] });
+			// MODチャンネル側にメッセージを送信
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('hail')
+						.setLabel('( ᐛ )')
+						.setStyle('PRIMARY'),
+				);
+			const embed_mod = new MessageEmbed()
+				.setColor('#56B482')
+				.setTitle('新しい申請が送信されました!')
+				.setDescription(`申請者:<@${userId}>`)
+				.setThumbnail(userAvater)
+				.addFields(
+					{ name: 'エディション', value: `${edition}版`, inline: true },	
+					{ name: 'MCID', value: `${mcid}`, inline: true }
+				);
+			await interaction.guild.channels.cache.get(modCh).send({ embeds: [embed_mod], components: [row] });
+		} else {
+			// もし申請チャンネル以外で送っていた場合にエラーを表示
+			const embed_error = new MessageEmbed()
+				.setColor('#E84136')
+				.setDescription(`<#${requestCh}>以外でこのコマンドを使うことはできません!`);
+				await interaction.reply({ embeds: [embed_error], ephemeral: true });
+		}
 	},
 }; 
--- deploy-commands.js
+++ deploy-commands.js
@@ -18,5 +18,5 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then(() => console.log('アプリケーションコマンドの登録に成功しました。'))
 	.catch(console.error);
