30a3b5b76b1e243b31100543fabdb149c3f60b35
null1017 <nonick.mc1017@gmail.com>
2022-03-31 16:40:55 +0900

    deploy-command.jsの修正
    ・TOKENのenv化に対応

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-// commandsフォルダに追加したコマンドを登録するために使われる
+// commandsフォルダに追加したコマンドを登録するために使われる (サーバーのみ)
 // コマンド更新の際に一回だけ使用すること(複数回やっても意味がない)
 
 const fs = require('node:fs');
@@ -6,6 +6,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config();
 
 const commands = []
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -15,7 +16,7 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('アプリケーションコマンドの登録に成功しました。'))
