0be6717743c22bd151814f65515e53385345c0a4
null1017 <nonick.mc1017@gmail.com>
2022-03-31 23:52:15 +0900

    Helpコマンド -修正
    ・絵文字部分が上手く表示されなかった問題を修正

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setTitle(`NoNICK's SERVER bot`)
             .setColor('#FFFFFF')
-            .setDescription(`**Minecraftサーバー運営のお手伝いをするBOT**\n<@${userId}>さん、こんにちは!\n<:slashcommand:959041066810638346>以下から利用可能なコマンドを確認できます!`)
+            .setDescription(`**Minecraftサーバー運営のお手伝いをするBOT**\n<@${userId}>さん、こんにちは!\n以下から利用可能なコマンドを確認できます!`)
             .addFields(
                 {name: '/help', value: '自身が使用可能なコマンドを確認します。'},
                 {name: '/request', value: `${serverName}への参加申請を送ります。`}
