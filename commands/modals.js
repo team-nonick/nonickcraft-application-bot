20cb1a95dc47aed1c4c91fdd20940ee3c8417b06
null1017 <nonick.mc1017@gmail.com>
2022-04-08 21:42:19 +0900

    Update modals.js
    色々追加した

--- commands/modals.js
+++ commands/modals.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const { requestCh, serverName } = require('../config.json');
+const { Modal, TextInputComponent } = require('discord-modals')
+const { Formatters } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,5 +11,21 @@ module.exports = {
         // const botAvater = ;
         const userId = interaction.user.id; //コマンドを打った人のidを取得
         interaction.reply({content: test,  ephemeral: true});
+        const modaltest = new Modal()
+        .setCustomId('customid')
+        .setTitle('申請フォーラム(テスト)')
+        .addComponents()
+            new TextInputComponent()
+            .setCustomId('textinput-customid1')
+            .setLabel('MCIDを入力してください')
+            .setStyle('SHORT')
+            .setMinLength(4)
+            .setMaxLength(10)
+            .setPlaceholder('ここに書けぇ')
+            .setRequired(true);
+        showModal(modaltest, {
+            client: client,
+            interaction: interaction 
+        })
 	},
 };
