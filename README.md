9b9b44aaf9f980785b802444c3e908b56971a05f
null1017 <nonick.mc1017@gmail.com>
2022-04-09 19:24:09 +0900

    1.12
    ・申請を受け付けるか選択できる「enable_Request」オプションを追加
    ・1.11にあった軽微な問題を修正
    ・バージョンアップに合わせたREADMEの変更

--- README.md
+++ README.md
@@ -9,7 +9,10 @@ Discord.js v13に追加されたスラッシュコマンドやボタン等を積
 
 * `/request` ... ホワイトリストに追加するための申請を送信することができます。指定したチャンネル以外では使用できず、使用すると申請情報がモデレーターチャンネルに転送されます。
 ![picture 2](https://media.discordapp.net/attachments/958791423161954445/959084240211939388/2869545b09517e0342b0c585727a2321e8ddb112f7cebf17c7e2301076822698.png)  
-![picture 1](https://media.discordapp.net/attachments/958791423161954445/959084239977074778/940a03c4e3ff9064c28a5b2529f04a9986ac2c7de0ef29ea4b8de6c36388bdeb.png)  
+![picture 1](https://media.discordapp.net/attachments/958791423161954445/959084239977074778/940a03c4e3ff9064c28a5b2529f04a9986ac2c7de0ef29ea4b8de6c36388bdeb.png)
+
+* また、コマンドの使用の許可を切り替えることも可能です。
+![picture 4](https://cdn.discordapp.com/attachments/958791423161954445/962296309174390804/unknown.png)
 
 * `/help` ...　DiscordBOTでお馴染みのコマンド。使用できるコマンドの一覧とバージョンを表示できます。v12以前のhelpコマンドに慣れている方のためのコマンドです。
 ![picture_3](https://media.discordapp.net/attachments/958791423161954445/959084124184903732/unknown.png)
@@ -41,6 +44,7 @@ npm install discord.js @discordjs/builders @discordjs/rest discord-api-types dot
     "modCh": "モデレーターチャンネルのID",
     "requestCh": "/request の使用を許可するチャンネルのID",
     "beplayerprefix": "GeyserMCでBEプレイヤーに設定している接頭辞(標準では.)"
+    "enable_Request": true //(falseにすることで申請コマンドの使用を使えないようにする)
 }
 ```
 同じく新規作成した`.env` にはDiscordBOTのtokenを入力します。
--- commands/request.js
+++ commands/request.js
@@ -3,7 +3,7 @@
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const { requestCh, modCh, serverName } = require('../config.json');
+const { requestCh, modCh, serverName, enable_Request } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -29,60 +29,67 @@ module.exports = {
 		const userAvater = interaction.user.avatarURL(); //コマンドを打った人のアバターURL
 		const sendCh = interaction.channelId //コマンドを発動したチャンネル
 
-		if (sendCh === requestCh) {
-			// 申請側にメッセージを送信
-			const embed = new MessageEmbed()
-				.setColor(`#5662F6`)
-				.setTitle('申請完了')
-				.setThumbnail(userAvater)
-				.setDescription(`以下の情報で申請を送信しました。\n**Tips:**登録には時間がかかる場合があります。\n__正しく申請を受け取るには、DMを開放しておいてください!__`)
-				.addFields(
-					{name: 'エディション', value: `${edition}版`, inline: true},	
-					{name: 'MCID', value: `${mcid}`, inline: true}
-				);
-			interaction.reply({ embeds: [embed] });
+		if (enable_Request) {
+			if (sendCh === requestCh) {
+				// 申請側にメッセージを送信
+				const embed = new MessageEmbed()
+					.setColor(`#5662F6`)
+					.setTitle('申請完了')
+					.setThumbnail(userAvater)
+					.setDescription(`以下の情報で申請を送信しました。\n**Tips:**登録には時間がかかる場合があります。\n__正しく申請を受け取るには、DMを開放しておいてください!__`)
+					.addFields(
+						{name: 'エディション', value: `${edition}版`, inline: true},	
+						{name: 'MCID', value: `${mcid}`, inline: true}
+					);
+				interaction.reply({ embeds: [embed] });
 
-			// MODチャンネル側に申請対応メッセージを送信
-			//↑につけるボタンボタン
-			const buttons = new MessageActionRow()
-				.addComponents(
-					new MessageButton()
-					.setCustomId('button_copy')
-					.setLabel('コマンドをコピー')
-					.setEmoji('📃')
-					.setStyle('PRIMARY'),
-				)
-				.addComponents(
-					new MessageButton()
-					.setCustomId('button_ok')
-					.setLabel('許可')
-					.setStyle('SUCCESS'),
-				)
-				.addComponents(
-					new MessageButton()
-					.setCustomId('button_ng')
-					.setLabel('拒否')
-					.setStyle('DANGER'),
-				);
-			
-			//埋め込み
-			const embed_mod = new MessageEmbed()
-				.setColor('#56B482')
-				.setTitle('申請 - 新しい申請が送信されました!')
-				.setDescription(`申請者:<@${userId}>`)
-				.setThumbnail(userAvater)
-				.addFields(
-					{name: 'ユーザーID', value: `${userId}` },
-					{name: 'エディション', value: `${edition}版`, inline: true},	
-					{name: 'MCID', value: `${mcid}`, inline: true}
-				);
-			await interaction.guild.channels.cache.get(modCh).send({ embeds: [embed_mod], components: [buttons] });
+				// MODチャンネル側に申請対応メッセージを送信
+				//↑につけるボタンボタン
+				const buttons = new MessageActionRow()
+					.addComponents(
+						new MessageButton()
+						.setCustomId('button_copy')
+						.setLabel('コマンドをコピー')
+						.setEmoji('📃')
+						.setStyle('PRIMARY'),
+					)
+					.addComponents(
+						new MessageButton()
+						.setCustomId('button_ok')
+						.setLabel('許可')
+						.setStyle('SUCCESS'),
+					)
+					.addComponents(
+						new MessageButton()
+						.setCustomId('button_ng')
+						.setLabel('拒否')
+						.setStyle('DANGER'),
+					);
+				
+				//埋め込み
+				const embed_mod = new MessageEmbed()
+					.setColor('#56B482')
+					.setTitle('申請 - 新しい申請が送信されました!')
+					.setDescription(`申請者:<@${userId}>`)
+					.setThumbnail(userAvater)
+					.addFields(
+						{name: 'ユーザーID', value: `${userId}` },
+						{name: 'エディション', value: `${edition}版`, inline: true},	
+						{name: 'MCID', value: `${mcid}`, inline: true}
+					);
+				await interaction.guild.channels.cache.get(modCh).send({ embeds: [embed_mod], components: [buttons] });
+			} else {
+				// もし申請チャンネル以外で送っていた場合にエラーを表示
+				const embed_error = new MessageEmbed()
+					.setColor('#E84136')
+					.setDescription(`<#${requestCh}>以外でこのコマンドを使うことはできません!`);
+				interaction.reply({embeds: [embed_error], ephemeral: true});
+			}
 		} else {
-			// もし申請チャンネル以外で送っていた場合にエラーを表示
-			const embed_error = new MessageEmbed()
+			const embed_not = new MessageEmbed()
 				.setColor('#E84136')
-				.setDescription(`<#${requestCh}>以外でこのコマンドを使うことはできません!`);
-			interaction.reply({embeds: [embed_error], ephemeral: true});
+				.setDescription(`只今、申請は受付を停止しています。`);
+			interaction.reply({embeds: [embed_not], ephemeral: true});
 		}
 	},
 }; 
