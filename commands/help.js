c7bc33fe183bf373192b9b143763f99339cadd47
null1017 <nonick.mc1017@gmail.com>
2022-04-11 16:51:22 +0900

    バージョンの表記の修正

--- commands/help.js
+++ commands/help.js
@@ -7,16 +7,16 @@ module.exports = {
 		.setName('help')
 		.setDescription('自身が使用可能なコマンドを確認します。'),
 	async execute(interaction) {
-        const userId = interaction.user.id; //コマンドを打った人のidを取得
+        const user_id = interaction.user.id; //コマンドを打った人のidを取得
         const embed = new MessageEmbed()
             .setTitle(botName)
             .setColor('#FFFFFF')
-            .setDescription(`**Minecraftサーバー運営のお手伝いをするBOT**\n<@${userId}>さん、こんにちは!\n以下から利用可能なコマンドを確認できます!`)
+            .setDescription(`**Minecraftサーバー運営のお手伝いをするBOT**\n<@${user_id}>さん、こんにちは!\n以下から利用可能なコマンドを確認できます!`)
             .addFields(
                 {name: '/help', value: '自身が使用可能なコマンドを確認します。'},
                 {name: '/request', value: `${serverName}への参加申請を送ります。`}
             )
-            .setFooter({text: `v1.0`});
+            .setFooter({text: `v1.12.2`});
         interaction.reply({embeds: [embed],  ephemeral: true});
 	},
 };
